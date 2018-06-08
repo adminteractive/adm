639fcde5639322d1f78b6cc244373f33fae22302
Märt Rang <rang501@gmail.com>
2018-06-08 16:08:47 +0300

    Use experimental paragraphs widget.

--- adm.profile
+++ adm.profile
@@ -4,3 +4,21 @@
  * Enables modules and site configuration for a standard site installation.
  */
 
+/**
+ * Implements hook_install_tasks().
+ *
+ * @param $install_state
+ *
+ * @return array
+ */
+function adm_install_tasks(&$install_state) {
+  // TODO add custom steps.
+  //$tasks = [];
+
+  //$tasks['\Drupal\adm\Form\SelectComponentsForm'] = [
+  //  'display_name' => t('Select Components'),
+  //  'type' => 'form',
+  //];
+
+  // return $tasks;
+}
---
+++ src/Form/SelectComponentsForm.php
@@ -0,0 +1,65 @@
+<?php
+namespace Drupal\adm\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class SelectComponentsForm extends FormBase {
+
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   *   The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'select_components_form';
+  }
+
+  /**
+   * Form constructor.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   *
+   * @return array
+   *   The form structure.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['#title'] = $this->t('Select components');
+    $form['components'] = [
+      '#type' => 'select',
+      '#options' => [
+        'media' => $this->t('Media'),
+        'webform' => $this->t('Webform'),
+        'search' => $this->t('Search'),
+      ],
+      '#multiple' => TRUE,
+    ];
+
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Next'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   *   An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // TODO: Implement submitForm() method.
+  }
+}
